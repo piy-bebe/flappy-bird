c71e0680ef6a50948f1169284b3398e702881194
piy-bebe <piy.bebe@gmail.com>
2025-05-17 17:06:55 +0300

    style: добавить стили для скиллов

--- game/core/shop.js
+++ game/core/shop.js
@@ -12,8 +12,24 @@ const getHtml = (selector) => {
 };
 
 const createSkill = ({ name, icon }) => {
+  const container = document.createElement('div');
+  container.classList.add('skills__container');
+
   const button = document.createElement('button');
   button.classList.add('skill');
+
+  const info = document.createElement('div');
+  info.classList.add('skills__info');
+
+  const text = document.createElement('p');
+  text.classList.add('skills__text');
+  text.textContent = `Моментальное падение - эта способность позволяет быстро снижать высоту прыжка, что может быть полезно в некоторых ситуациях.`;
+
+  const coins = document.createElement('p');
+  coins.classList.add('skills__coins');
+  coins.textContent = `10 Coins`;
+  info.append(text, coins);
+
   const span = document.createElement('span');
   span.className = 'material-symbols-outlined skill-size';
   span.textContent = icon;
@@ -30,7 +46,8 @@ const createSkill = ({ name, icon }) => {
 
   button.append(span);
 
-  return button;
+  container.append(button, info);
+  return container;
 };
 
 export const skills = () => {
